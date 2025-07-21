8a06d283a0c6523f646d6039abc435e6587124a6
Magalakshmi2122 <magalakshmis226@gmail.com>
2025-07-21 11:47:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,15 +12,13 @@ st.write(
 
 name_on_order= st.text_input("Name On Smoothie")
 st.write("The name on your smoothie will be",name_on_order)
-cnx=st.connection("snowflake")
-session=cnx.session()
+
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect(
-    'Choose upto 5 ingredients.:'
-    , my_dataframe
-    ,max_selections=5
+    'Choose upto 5 ingredients.:', my_dataframe
 )
 
 if ingredients_list:
@@ -30,22 +29,9 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    # st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     # st.stop()
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
-      dora_insert = """
-    INSERT INTO smoothies.public.orders (ingredients, name_on_order, order_filled, hash_ing)
-    VALUES 
-      ('combo_for_Kevin', 'Kevin', FALSE, 7976616299844859825),
-      ('combo_for_Divya', 'Divya', TRUE, -6112358379204300652),
-      ('combo_for_Xi', 'Xi', TRUE, 1016924841131818535);
-    """
-    if st.button("Insert DORA Test Orders"):
-        session.sql(dora_insert).collect()
-        st.success("DORA test orders inserted!")
-
-#         st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
-# import requests
-# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response)
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
